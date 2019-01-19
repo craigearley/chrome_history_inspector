e9db28839fabd11f76475119ae589d51e5870d12
Craig Earley <io@craigearley.com>
2019-01-19 13:55:48 -0500

    corrects domain groupby so there is only one count column

--- comb-history.py
+++ comb-history.py
@@ -12,8 +12,8 @@ history_df = pd.read_csv(filename)
 history_df.columns = ['timestamp','url']
 history_df['domain'] = history_df.url.str.replace('http://','').str.replace('https://','').str.replace('www.','')
 history_df['domain'] = history_df.domain.str.split(delim).str.get(0)
+print(history_df)
 
-counts = history_df.groupby('domain').count()
-counts.columns = ['domain','count']
+counts = history_df.groupby(['domain']).size().reset_index()
+print(counts)
 
-print(counts.sort_values(['count'], ascending=False).head(ROWS_TO_DISPLAY))
