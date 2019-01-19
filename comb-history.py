298a6103431d90057105f3cffdb18dd65afe0295
Craig Earley <io@craigearley.com>
2019-01-19 14:35:19 -0500

    prettifies bar chart

--- comb-history.py
+++ comb-history.py
@@ -4,6 +4,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# read data
+
 delim = '/'
 ROWS_TO_DISPLAY = 20
 
@@ -13,13 +15,19 @@ f = open(filename)
 history_df = pd.read_csv(filename)
 history_df.columns = ['timestamp','url']
 history_df['domain'] = history_df.url.str.replace('http://','').str.replace('https://','').str.replace('www.','')
-history_df['domain'] = history_df.domain.str.split(delim).str.get(0)
+history_df['domain'] = history_df.domain.str.split(delim).str.get(0).str.rsplit('.', 1).str.get(0)
 
 counts = history_df.groupby(['domain']).size().reset_index(name='count')
 print(counts.sort_values('count', ascending=False).head(ROWS_TO_DISPLAY))
 
+# plots
+
 sns.set(style="whitegrid")
 
-counts.sort_values('count',ascending=False).head(ROWS_TO_DISPLAY).plot(kind='bar',x='domain',y='count')
-
+counts.sort_values('count',ascending=False).head(ROWS_TO_DISPLAY).plot(kind='bar',x='domain',y='count', legend=None)
+plt.xlabel('site')
+plt.ylabel('visit count')
+plt.title('Your ' + str(ROWS_TO_DISPLAY) + ' Most Visited Sites')
+plt.xticks(rotation=60)
+plt.tight_layout()
 plt.show()
