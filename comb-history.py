007f52d7c30cbb0d270aad0a582a660a6a7bc1f8
Craig Earley <io@craigearley.com>
2019-01-19 14:41:59 -0500

    increases image size

--- comb-history.py
+++ comb-history.py
@@ -18,16 +18,23 @@ history_df['domain'] = history_df.url.str.replace('http://','').str.replace('htt
 history_df['domain'] = history_df.domain.str.split(delim).str.get(0).str.rsplit('.', 1).str.get(0)
 
 counts = history_df.groupby(['domain']).size().reset_index(name='count')
-print(counts.sort_values('count', ascending=False).head(ROWS_TO_DISPLAY))
+#print(counts.sort_values('count', ascending=False).head(ROWS_TO_DISPLAY))
 
 # plots
 
 sns.set(style="whitegrid")
-
 counts.sort_values('count',ascending=False).head(ROWS_TO_DISPLAY).plot(kind='bar',x='domain',y='count', legend=None)
+
+figure = plt.gcf()
+figure.set_size_inches(8,6)
+
 plt.xlabel('site')
 plt.ylabel('visit count')
+
 plt.title('Your ' + str(ROWS_TO_DISPLAY) + ' Most Visited Sites')
+
 plt.xticks(rotation=60)
+
 plt.tight_layout()
-plt.show()
+
+plt.savefig('most_visited_sites.png')
